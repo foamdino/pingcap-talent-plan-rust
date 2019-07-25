9d35ee152d7d5ee16505958804c245755d0c1b93
Kev Jackson <foamdino@gmail.com>
2019-07-25 11:33:30 +0100

    get tests compiling etc

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,34 @@
 //! `kvs` is a library for a distributed key-val store (in the same sphere as consul/cockroach/etcd)
 
 use std::collections::BTreeMap;
+use std::path::Path;
+use std::error;
+use std::result;
+use std::fmt;
 
 /// Defines the KvStore struct - right now it's minimal
 #[derive(Default)]
 pub struct KvStore {
     store: BTreeMap<String, String>,
+}
+
+/// type alias for Result
+pub type Result<T> = result::Result<T, KvsError>;
+
+/// Defines a custom error type
+#[derive(Debug)]
+pub struct KvsError;
+
+impl fmt::Display for KvsError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "error while interacting with kvs")
+    }
+}
+
+impl error::Error for KvsError {
+    fn cause(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
 }
 
 impl KvStore {
@@ -29,6 +52,13 @@ impl KvStore {
         }
     }
 
+    /// Returns a previously persisted KvStore opened from a file
+    pub fn open(_path: &Path) -> Result<KvStore> {
+        Ok(KvStore {
+            store: BTreeMap::new(),
+        })
+    }
+
     /// Sets a value for a given key
     ///
     /// # Example
@@ -37,8 +67,9 @@ impl KvStore {
     /// let mut store = KvStore::new();
     /// store.set("key1".to_owned(), "value1".to_owned());
     /// ```
-    pub fn set(&mut self, k: String, v: String) {
+    pub fn set(&mut self, k: String, v: String) -> Result<()> {
         self.store.insert(k, v);
+        Ok(())
     }
 
     /// Gets a value for a given key
@@ -51,10 +82,10 @@ impl KvStore {
     /// let v1 = store.get("key1".to_owned());
     /// assert_eq!("value1".to_owned(), v1.unwrap());
     /// ```
-    pub fn get(&self, k: String) -> Option<String> {
+    pub fn get(&self, k: String) -> Result<Option<String>> {
         match self.store.get(&k) {
-            Some(val) => Some(val.clone()),
-            None => None,
+            Some(val) => Ok(Some(val.clone())),
+            None => Err(KvsError),
         }
     }
 
@@ -69,7 +100,8 @@ impl KvStore {
     /// assert_eq!("value1".to_owned(), v1.unwrap());
     /// store.remove("key1".to_owned());
     /// ```
-    pub fn remove(&mut self, k: String) {
+    pub fn remove(&mut self, k: String) -> Result<String> {
         self.store.remove(&k);
+        Ok("removed".to_owned())
     }
 }
